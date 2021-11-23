93b88da4fdd48033bc8b9dd02b0f2dc43df85988
Thomas Junk <trj@fnal.gov>
2021-11-23 17:25:32 -0600

    make more portable

--- first_try/defvars.sh
+++ first_try/defvars.sh
@@ -2,7 +2,7 @@
 
 TARGETDIR=/build/trj
 UNSPLIT_SOURCE=${TARGETDIR}/splitter11/srcs
-SCRIPTDIR=/nashome/t/trj/split
+SCRIPTDIR=/nashome/t/trj/split/split_dunetpc/first_try
 SPLITDIR=${TARGETDIR}/split_target
 SPLITTARGET=${SPLITDIR}/srcs
 SPLITPRODUCTS=${TARGETDIR}/splitproducts
--- first_try/mytest11build_newdev.sh
+++ first_try/mytest11build_newdev.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
+source ./defvars.sh
+
 LARSOFT_VERSION=v09_36_00
 COMPILER=e20
 DIRECTORY=splitter11
 USERNAME=`whoami`
-HDIR=/build
 
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 
-cd ${HDIR}/${USERNAME}
+cd ${TARGETDIR}
 touch ${DIRECTORY}
 rm -rf ${DIRECTORY}
 mkdir ${DIRECTORY}
 cd ${DIRECTORY}
 setup larsoft ${LARSOFT_VERSION} -q ${COMPILER}:prof
 mrb newDev -q ${COMPILER}:prof
-source ${HDIR}/${USERNAME}/${DIRECTORY}/localProducts*/setup
+source ${TARGETDIR}/${DIRECTORY}/localProducts*/setup
 mkdir work
 cd srcs
 mrb g -t ${LARSOFT_VERSION} dunetpc
